0212540bb9fa1a3cbaa7847fadf230d5d4a7c3d6
Martin Hamel <martin@ma4s.org>
2024-10-29 16:29:47 +0000

    secondary .35 residential + bicycle yes .7

--- src/db/edge.rs
+++ src/db/edge.rs
@@ -142,7 +142,11 @@ impl Edge {
                 1. / 0.3
             }
         } else if self.tags.get("highway") == Some(&"residential".to_string()) {
-            1. / 0.6
+            if self.tags.get("bicycle") == Some(&"yes".to_string()) {
+                1. / 0.7
+            } else {
+                1. / 0.6
+            }
         } else if self.tags.get("highway") == Some(&"tertiary".to_string()) {
             1. / 0.5
         } else if self.tags.get("higway") == Some(&"tertiary_link".to_string()) {
@@ -151,20 +155,18 @@ impl Edge {
             && self.tags.get("footway") == Some(&"crossing".to_string())
         {
             1. / 0.5
-        } else if self.tags.get("highway") == Some(&"secondary_link".to_string()) {
-            1. / 0.4
+        } else if self.tags.get("bicycle") == Some(&"yes".to_string()) {
+            1. / 0.5
         } else if self.tags.get("highway") == Some(&"unclassified".to_string()) {
             1. / 0.4
         } else if self.tags.get("bicycle") == Some(&"designated".to_string()) {
             1. / 0.4
-        } else if self.tags.get("bicycle") == Some(&"yes".to_string()) {
-            1. / 0.5
-        } else if self.tags.get("bicycle") == Some(&"yes".to_string()) {
-            1. / 0.5
         } else if self.tags.get("highway") == Some(&"path".to_string()) {
             1. / 0.4
         } else if self.tags.get("highway") == Some(&"secondary".to_string()) {
-            1. / 0.4
+            1. / 0.35
+        } else if self.tags.get("highway") == Some(&"secondary_link".to_string()) {
+            1. / 0.35
         } else if self.tags.get("cycleway") == Some(&"separate".to_string()) {
             1. / 0.4
         } else if self.tags.get("cycleway:both") == Some(&"separate".to_string()) {
