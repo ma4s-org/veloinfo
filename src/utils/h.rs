07f7e24b85924669e4bf4424a6441110f0386e66
Martin Hamel <martin@ma4s.org>
2025-01-03 18:18:30 +0000

    private no

--- src/utils/h.rs
+++ src/utils/h.rs
@@ -34,7 +34,9 @@ impl H for HMoyen {
             return 1. / 0.05;
         }
 
-        let mut cost = if edge.edge.tags.get("bicycle") == Some(&"no".to_string()) {
+        let mut cost = if edge.edge.tags.get("bicycle") == Some(&"no".to_string())
+            || edge.edge.tags.get("access") == Some(&"private".to_string())
+        {
             1. / 0.05
         } else if edge.edge.tags.get("higway") == Some(&"proposed".to_string()) {
             1. / 0.05
