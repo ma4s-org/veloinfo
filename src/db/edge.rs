3567b2d00622715a80d1ed0b5379e0a185cf39b1
Martin Hamel <martin@ma4s.org>
2025-01-03 16:54:18 +0000

    shortcut

--- src/db/edge.rs
+++ src/db/edge.rs
@@ -6,7 +6,6 @@ use std::{
 
 use crate::utils::h::H;
 use crate::{db::utils::Score, utils::h::get_h_moyen};
-use futures::future::join_all;
 use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use sqlx::Postgres;
@@ -66,9 +65,126 @@ impl PartialEq for Edge {
 }
 
 #[derive(Debug, Clone, Eq, Hash)]
+pub enum Neighbour {
+    EdgePoint(EdgePoint),
+    Shortcut(Shortcut),
+}
+
+impl Neighbour {
+    pub fn length(&self) -> f64 {
+        match self {
+            Neighbour::EdgePoint(edge_point) => edge_point.edge.length,
+            Neighbour::Shortcut(shortcut) => shortcut.length,
+        }
+    }
+
+    pub fn get_cost(&self, h: &Box<dyn H>) -> f64 {
+        match self {
+            Neighbour::EdgePoint(edge_point) => h.get_cost(edge_point),
+            Neighbour::Shortcut(shortcut) => shortcut.cost,
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        match self {
+            Neighbour::EdgePoint(edge_point) => edge_point
+                .edge
+                .tags
+                .get("name")
+                .unwrap_or(&"".to_string())
+                .clone(),
+            Neighbour::Shortcut(shortcut) => shortcut.name.clone(),
+        }
+    }
+
+    pub fn get_points(&self) -> Vec<Point> {
+        match self {
+            Neighbour::EdgePoint(edge_point) => match edge_point.point {
+                SourceOrTarget::Source => vec![Point {
+                    lng: edge_point.edge.lon1,
+                    lat: edge_point.edge.lat1,
+                    way_id: edge_point.edge.way_id,
+                    node_id: edge_point.edge.source,
+                    length: edge_point.edge.length,
+                }],
+                SourceOrTarget::Target => vec![Point {
+                    lng: edge_point.edge.lon2,
+                    lat: edge_point.edge.lat2,
+                    way_id: edge_point.edge.way_id,
+                    node_id: edge_point.edge.target,
+                    length: edge_point.edge.length,
+                }],
+            },
+            Neighbour::Shortcut(shortcut) => shortcut.path.clone(),
+        }
+    }
+
+    pub async fn get_neighbors(&self, conn: &sqlx::Pool<Postgres>) -> Vec<ARc<Neighbour>> {
+        match self {
+            Neighbour::EdgePoint(edge_point) => edge_point.get_neighbors(conn).await,
+            Neighbour::Shortcut(shortcut) => shortcut.target.get_neighbors(conn).await,
+        }
+    }
+
+    pub fn h(&self, h: &Box<dyn H>, end_edge: &Neighbour) -> f64 {
+        let end_edge = match end_edge {
+            Neighbour::EdgePoint(edge_point) => edge_point,
+            Neighbour::Shortcut(shortcut) => &shortcut.target,
+        };
+        match self {
+            Neighbour::EdgePoint(edge_point) => h.h(edge_point, end_edge),
+            Neighbour::Shortcut(shortcut) => h.h(&shortcut.target, end_edge),
+        }
+    }
+
+    pub fn get_begining_point(&self) -> ARc<EdgePoint> {
+        match self {
+            Neighbour::EdgePoint(edge_point) => ARc::new(edge_point.clone()),
+            Neighbour::Shortcut(shortcut) => shortcut.source.clone(),
+        }
+    }
+
+    pub fn get_ending_point(&self) -> ARc<EdgePoint> {
+        match self {
+            Neighbour::EdgePoint(edge_point) => ARc::new(EdgePoint {
+                edge: edge_point.edge.clone(),
+                cost: edge_point.cost,
+                point: match edge_point.point {
+                    SourceOrTarget::Source => SourceOrTarget::Target,
+                    SourceOrTarget::Target => SourceOrTarget::Source,
+                },
+            }),
+            Neighbour::Shortcut(shortcut) => shortcut.target.clone(),
+        }
+    }
+}
+
+impl PartialEq for Neighbour {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Neighbour::EdgePoint(edge_point1), Neighbour::EdgePoint(edge_point2)) => {
+                edge_point1 == edge_point2
+            }
+            (Neighbour::Shortcut(shortcut1), Neighbour::Shortcut(shortcut2)) => {
+                shortcut1 == shortcut2
+            }
+            _ => false,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct EdgePoint {
     pub edge: Edge,
     pub point: SourceOrTarget,
+    pub cost: Option<f64>,
+}
+
+impl Hash for EdgePoint {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.edge.hash(state);
+        self.point.hash(state);
+    }
 }
 
 #[derive(Debug, Clone, Eq, Hash)]
@@ -77,12 +193,28 @@ pub enum SourceOrTarget {
     Target,
 }
 
+#[derive(Debug, Clone)]
 pub struct Shortcut {
     pub source: ARc<EdgePoint>,
     pub target: ARc<EdgePoint>,
-    pub path: Vec<ARc<EdgePoint>>,
+    pub name: String,
+    pub path: Vec<Point>,
     pub length: f64,
-    pub score: f64,
+    pub cost: f64,
+}
+
+impl PartialEq for Shortcut {
+    fn eq(&self, other: &Self) -> bool {
+        self.source == other.source && self.target == other.target
+    }
+}
+
+impl Eq for Shortcut {}
+impl Hash for Shortcut {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.source.hash(state);
+        self.target.hash(state);
+    }
 }
 
 impl PartialEq for SourceOrTarget {
@@ -101,6 +233,8 @@ impl PartialEq for EdgePoint {
     }
 }
 
+impl Eq for EdgePoint {}
+
 impl EdgePoint {
     pub fn get_node_id(&self) -> i64 {
         match self.point {
@@ -109,7 +243,7 @@ impl EdgePoint {
         }
     }
 
-    pub async fn get_neighbors(&self, conn: &sqlx::Pool<Postgres>) -> Vec<ARc<EdgePoint>> {
+    pub async fn get_neighbors(&self, conn: &sqlx::Pool<Postgres>) -> Vec<ARc<Neighbour>> {
         let mut cache = NEIGHBORS_CACHE.lock().await;
         let node_id = self.get_node_id();
         if cache.contains_key(&node_id) {
@@ -141,19 +275,21 @@ impl EdgePoint {
         .await
         {
             Ok(results) => {
-                let result: Vec<ARc<EdgePoint>> = results
+                let result: Vec<ARc<Neighbour>> = results
                     .into_iter()
                     .map(|edge: Edge| {
                         if edge.source == node_id {
-                            return ARc::new(EdgePoint {
+                            return ARc::new(Neighbour::EdgePoint(EdgePoint {
+                                cost: None,
                                 edge,
                                 point: SourceOrTarget::Target,
-                            });
+                            }));
                         } else {
-                            return ARc::new(EdgePoint {
+                            return ARc::new(Neighbour::EdgePoint(EdgePoint {
+                                cost: None,
                                 edge,
                                 point: SourceOrTarget::Source,
-                            });
+                            }));
                         }
                     })
                     .collect();
@@ -169,7 +305,7 @@ impl EdgePoint {
 }
 
 lazy_static! {
-    static ref NEIGHBORS_CACHE: Mutex<HashMap<i64, Vec<ARc<EdgePoint>>>> =
+    static ref NEIGHBORS_CACHE: Mutex<HashMap<i64, Vec<ARc<Neighbour>>>> =
         Mutex::new(HashMap::new());
 }
 
@@ -191,12 +327,12 @@ impl Edge {
         let mut min_in_open_set = BTreeMap::new();
         open_set.insert(start_edge.clone());
         min_in_open_set.insert(Score(0.), start_edge.clone());
-        let mut came_from: HashMap<ARc<EdgePoint>, ARc<EdgePoint>> = HashMap::new();
+        let mut came_from: HashMap<ARc<Neighbour>, ARc<Neighbour>> = HashMap::new();
         // g_score is the shortest distance from the start node to the current node
-        let mut g_score: HashMap<ARc<EdgePoint>, f64> = HashMap::new();
+        let mut g_score: HashMap<ARc<Neighbour>, f64> = HashMap::new();
         g_score.insert(start_edge.clone(), 0.0);
         // f_score is the shortest distance from the start node to the end node
-        let mut f_score: HashMap<ARc<EdgePoint>, f64> = HashMap::new();
+        let mut f_score: HashMap<ARc<Neighbour>, f64> = HashMap::new();
         f_score.insert(start_edge.clone(), 0.0);
 
         // a* algorithm
@@ -212,55 +348,21 @@ impl Edge {
             let current = first_min_entry.get().clone();
             // if we are at the end, return the path
             if current == end_edge {
-                let mut current = &end_edge;
-                let mut path = vec![];
-                while current != &start_edge {
-                    path.push(current);
-                    current = came_from.get(current).unwrap();
-                }
-                path.push(&start_edge);
-                path.reverse();
-                let promises = path
-                    .iter()
-                    .map(|edge| async {
-                        match edge.point {
-                            SourceOrTarget::Source => {
-                                return Point {
-                                    lng: edge.edge.lon1,
-                                    lat: edge.edge.lat1,
-                                    way_id: edge.edge.way_id,
-                                    node_id: edge.edge.source,
-                                    length: edge.edge.length,
-                                }
-                            }
-                            SourceOrTarget::Target => {
-                                return Point {
-                                    lng: edge.edge.lon2,
-                                    lat: edge.edge.lat2,
-                                    way_id: edge.edge.way_id,
-                                    node_id: edge.edge.target,
-                                    length: edge.edge.length,
-                                }
-                            }
-                        }
-                    })
-                    .collect::<Vec<_>>();
-                let points = join_all(promises).await;
-                return points;
+                return Edge::extract_end_path(end_edge, start_edge, came_from, g_score).await;
             }
             open_set.remove(&current);
             first_min_entry.remove();
             let neighbors = current.get_neighbors(conn).await;
             for neighbor in neighbors.iter() {
                 let tentative_g_score = g_score.get(&current).expect("current should have a score")
-                    + neighbor.edge.length * h.get_cost(neighbor);
+                    + neighbor.length() * neighbor.get_cost(&h);
                 let neighbord_g_score = g_score.get(neighbor);
                 if neighbord_g_score.is_none() || &tentative_g_score < neighbord_g_score.unwrap() {
                     came_from.insert(neighbor.clone(), current.clone());
                     g_score.insert(neighbor.clone(), tentative_g_score);
                     f_score.insert(
                         neighbor.clone(),
-                        tentative_g_score + h.h(&neighbor, &end_edge),
+                        tentative_g_score + neighbor.h(&h, &end_edge),
                     );
                     if !open_set.contains(neighbor) {
                         open_set.insert(neighbor.clone());
@@ -271,6 +373,64 @@ impl Edge {
             }
         }
         vec![]
+    }
+
+    async fn extract_end_path(
+        end_edge: ARc<Neighbour>,
+        start_edge: ARc<Neighbour>,
+        came_from: HashMap<ARc<Neighbour>, ARc<Neighbour>>,
+        g_score: HashMap<ARc<Neighbour>, f64>,
+    ) -> Vec<Point> {
+        let mut current = &end_edge;
+        let mut path = vec![];
+        let mut current_shortcut = Shortcut {
+            source: start_edge.get_begining_point(),
+            target: end_edge.get_ending_point(),
+            name: current.get_name(),
+            cost: *g_score.get(&end_edge).unwrap(),
+            length: start_edge.length(),
+            path: current.get_points(),
+        };
+        println!("Curent shortcut: {:?}", current_shortcut);
+        while current != &start_edge {
+            path.push(current);
+            let next = came_from.get(current).unwrap();
+            if let Neighbour::Shortcut(shortcut) = &**next {
+                println!("Shortcut: {:?}", shortcut);
+            }
+            if current_shortcut.name == next.get_name() {
+                current_shortcut.length += next.length();
+                current_shortcut.path.extend(next.get_points());
+                current_shortcut.cost = *g_score.get(next).unwrap();
+                current_shortcut.target = next.get_ending_point();
+            } else {
+                // add shortcut to cash
+                NEIGHBORS_CACHE.lock().await.insert(
+                    current_shortcut.source.get_node_id(),
+                    vec![ARc::new(Neighbour::Shortcut(current_shortcut.clone()))],
+                );
+                current_shortcut = Shortcut {
+                    source: start_edge.get_begining_point(),
+                    target: end_edge.get_ending_point(),
+                    name: next.get_name(),
+                    cost: *g_score.get(&end_edge).unwrap(),
+                    length: start_edge.length(),
+                    path: next.get_points(),
+                };
+            }
+            current = next;
+        }
+        path.push(&start_edge);
+        path.reverse();
+        let points = path
+            .iter()
+            .map(|destination| {
+                let points = destination.get_points();
+                points
+            })
+            .flatten()
+            .collect::<Vec<_>>();
+        return points;
     }
 
     pub async fn find_closest_node(
@@ -314,7 +474,7 @@ impl Edge {
     pub async fn get(
         node_id: i64,
         conn: &sqlx::Pool<Postgres>,
-    ) -> Result<ARc<EdgePoint>, sqlx::Error> {
+    ) -> Result<ARc<Neighbour>, sqlx::Error> {
         let edge: Result<Edge, _> = sqlx::query_as(
             r#"SELECT
                 e.id,
@@ -345,7 +505,11 @@ impl Edge {
                 } else {
                     SourceOrTarget::Target
                 };
-                Ok(ARc::new(EdgePoint { edge, point }))
+                Ok(ARc::new(Neighbour::EdgePoint(EdgePoint {
+                    edge,
+                    point,
+                    cost: None,
+                })))
             }
             Err(e) => Err(e),
         }
