f447bf2f8c2bda79b00e8f0ac40d983d8a5c76dc
Martin Hamel <martin@ma4s.org>
2025-01-10 19:13:15 +0000

    score -1 = 1

--- src/utils/h.rs
+++ src/utils/h.rs
@@ -166,7 +166,9 @@ impl H for HMoyen {
         }
         let score = match edge.edge.score {
             Some(score) => {
-                if score <= 0. {
+                if score <= -0.9 {
+                    1.
+                } else if score <= 0. {
                     0.01
                 } else {
                     score
