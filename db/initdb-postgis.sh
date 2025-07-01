3808f8b1a0a845b3a68a352291065813c70cf819
Martin Hamel <martin@ma4s.org>
2025-07-01 20:14:30 +0000

    Dockerfiledb

---
+++ db/initdb-postgis.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+# Create the 'template_postgis' template db
+"${psql[@]}" <<- 'EOSQL'
+CREATE DATABASE template_postgis IS_TEMPLATE true;
+EOSQL
