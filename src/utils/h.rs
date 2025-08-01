3b503887f0e94671d3bab4bfd4cd258583182fb0
Martin Hamel <martin@ma4s.org>
2025-08-01 14:42:59 +0000

    Abandoned routes 0.001

--- src/utils/h.rs
+++ src/utils/h.rs
@@ -59,10 +59,10 @@ impl H for HMoyen {
             || edge.edge.tags.get("access") == Some(&"customers".to_string())
         {
             1. / 0.05
-        } else if edge.edge.tags.get("higway") == Some(&"proposed".to_string())
+        } else if edge.edge.tags.get("highway") == Some(&"proposed".to_string())
             || edge.edge.tags.get("abandoned") == Some(&"yes".to_string())
         {
-            1. / 0.05
+            1. / 0.001
         } else if edge.edge.tags.get("informal") == Some(&"yes".to_string()) {
             1. / 0.05
         } else if edge.edge.tags.get("highway") == Some(&"steps".to_string()) {
