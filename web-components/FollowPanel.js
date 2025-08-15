c646e17b812c284fc53b5302d5effdc21eaf5e4b
Martin Hamel <martin@ma4s.org>
2025-08-15 16:17:45 +0000

    Regarde 150m devant quand on est sur follow

--- web-components/FollowPanel.js
+++ web-components/FollowPanel.js
@@ -96,7 +96,7 @@ class FollowPanel extends HTMLElement {
                         longitude,
                         coord[1],
                         coord[0]
-                    ) >= 0.1
+                    ) >= 0.15
                 );
             hundredMeterAwayIndex = hundredMeterAwayIndex === -1
                 ? coordinates.length - 1
