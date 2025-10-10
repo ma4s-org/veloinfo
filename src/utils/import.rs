18171a925ed78635a2693b250ad22ac37d59a81b
Martin Hamel <martin@ma4s.org>
2025-10-10 07:43:20 -0400

    more import output

--- src/utils/import.rs
+++ src/utils/import.rs
@@ -7,9 +7,14 @@ pub async fn import(conn: &PgPool) {
     Edge::clear_all_cache().await;
     println!("Importing data");
     match Command::new("./import.sh").output() {
-        Ok(_) => {}
+        Ok(output) => {
+            if !output.status.success() {
+                println!("Error1 importing: {}", String::from_utf8_lossy(&output.stderr));
+            }
+            println!("{}", String::from_utf8_lossy(&output.stdout));
+        }
         Err(e) => {
-            println!("Error importing: {:?}", e);
+            println!("Error2 importing: {:?}", e);
         }
     }
 
